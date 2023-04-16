58b523c64715530d1ce5d455d094ca0fd83d267c
BagelSamurai <128832460+BagelSamurai@users.noreply.github.com>
2023-04-16 14:50:44 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+// Background
+window.onload= function() {
+    Particles.init
+  ({
+      selector: '.background',
+      color: ['#cceafc', "#ddfccc", '#fcd9fc'],
+      maxParticles: 50,
+      connectParticles: true,
+      sizeVariations: 1
+  
+
+    });
+  };
